0453bb8660d5f5267cee50daed9e55f949048895
Zaman <zamanehsani@gmail.com>
2021-03-27 12:46:17 +0400

    removed the basedir

--- Aldeyar/Aldeyar/settings.py
+++ Aldeyar/Aldeyar/settings.py
@@ -144,7 +144,7 @@ AWS_S3_FILE_OVERWRITE = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
