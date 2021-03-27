21c5e6a9d8c6eb4cc4aff199f79606c7547747c4
Zaman <zamanehsani@gmail.com>
2021-03-27 10:27:10 +0400

    os is import above

--- Aldeyar/Aldeyar/settings.py
+++ Aldeyar/Aldeyar/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -152,8 +152,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL =''
 LOGOUT_REDIRECT_URL = 'login'
-
-import os
 
 MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
 MEDIA_URL  = '/media/'
