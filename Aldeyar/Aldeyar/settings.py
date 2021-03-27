c1bc009d1c785e8805e5d4e3b5a1522c830d7fda
Zaman <zamanehsani@gmail.com>
2021-03-27 10:35:11 +0400

    added to allowed host

--- Aldeyar/Aldeyar/settings.py
+++ Aldeyar/Aldeyar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6#_=l2_k5eza(7vlh*%rrn(+w06rxupgahiy)2yh+8=gxk9cta'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.141', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.141', 'aldeyar.herokuapp.com']
 
 
 # Application definition
